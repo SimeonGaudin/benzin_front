7c124626b533c81b2bd300ef7e060f2ec0f0cc1a
kadwalon LOBET <Kadwalon.lobet@orange.Fr>
2024-11-20 15:59:48 +0100

    router connexion

--- benzin_front/src/app/app.routes.ts
+++ benzin_front/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent,},
-    //{ path: 'connexion', component: ConnexionComponent },
     { path: 'inscription', component: InscriptionComponent },
+    { path: 'connexion', component: ConnexionComponent },
   
 ];
